67eed41e5d06935e31c11b4629beaa1f8745b305
ZZQ <zzgaming@list.ru>
2020-06-17 19:43:19 +1000

    Add getArticles method

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,26 @@
 import Fetch from 'https://cdn.pika.dev/@economia/feed-to-json-promise@^1.8.2';
 import EventEmitter from 'https://raw.githubusercontent.com/denoland/deno/master/std/node/events.ts'
 
-export class Feed extends EventEmitter {
-    private tools: any;
+export default class RssFeed extends EventEmitter {
+    private tools: {
+        parseXmlToJson(data: string): any
+    };
     urls: string[]
-    protected _interval: number;
+    private _interval: number;
 
     constructor(urls?: string[], interval?: number) {
         super();
         this.tools = new Fetch()
         this.urls = urls || []
         this._interval = (interval || 60) * 1000
+    }
+
+    getArticles(url: string) {
+        return fetch(url)
+            .then(data => data.text())
+            .then(async rawXml => {
+                const xml = await this.tools.parseXmlToJson(rawXml)
+                return xml.rss.channel[0]
+            })
     }
 }
