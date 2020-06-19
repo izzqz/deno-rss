96a1aa4943eba0be1dbd22cc888fe0ecac33a48a
ZZQ <zzgaming@list.ru>
2020-06-19 22:40:19 +1000

    Add stopListening method and fix interval context

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import Fetch from 'https://cdn.pika.dev/@economia/feed-to-json-promise@^1.8.2';
-import EventEmitter from 'https://raw.githubusercontent.com/denoland/deno/master/std/node/events.ts'
+import EventEmitter from 'https://raw.githubusercontent.com/denoland/deno/master/std/node/events.ts';
 import { Cache } from "./types/cache.interface.ts";
 import { Article } from "./types/article.interface.ts";
 
@@ -28,20 +28,20 @@ export default class RssFeed extends EventEmitter {
                 const xml = await this.tools.parseXmlToJson(rawXml)
                     .catch(this.emitError)
 
-                return xml.rss.channel[0].item.map((item: any) => {
-                    item.title = item.title[0]
-                    return item
-                })
+                return xml.rss.channel[0].item
+                    .map((item: any) => {
+                        item.title = item.title[0]
+                        return item
+                    })
             })
             .catch(this.emitError)
     }
 
     checkUpdate(url: string, cacheOnly?: boolean): void {
-
         this.getAllArticles(url)
             .then((articles) => {
                 for (const article of articles) {
-                    if (!this.cacheHasTitle(url, article.title) && !cacheOnly) {
+                    if (!this.cached(url, article.title) && !cacheOnly) {
                         this.emit('update', article)
                     } else break;
                 }
@@ -53,12 +53,8 @@ export default class RssFeed extends EventEmitter {
 
     checkAllUpdates(): void {
         const requests = this.urls.map(url => this.checkUpdate(url))
-        Promise.allSettled(requests)
-            .then(results => {
-                for (const result of results) {
-
-                }
-            })
+        Promise.all(requests)
+            .catch(this.emitError)
     }
 
     private cacheAll(): void {
@@ -69,17 +65,20 @@ export default class RssFeed extends EventEmitter {
                     if (result.status === 'rejected') {
                         this.emitError(result.reason)
                     }
-                    if (result.status === 'fulfilled') {
-                        console.log(result)
-                        Deno.exit(0)
-                    }
                 }
             })
     }
     
     startListening(): void {
         this.cacheAll()
-        this.timerId = setInterval(this.checkAllUpdates, this._interval)
+        this.timerId = setInterval(this.checkAllUpdates.bind(this), this._interval)
+    }
+
+    stopListening(): void {
+        if (this.timerId == 0) {
+            clearInterval(this.timerId)
+            this.timerId = 0
+        }
     }
 
     private emitError(err: string | ErrorEvent) {
@@ -93,7 +92,8 @@ export default class RssFeed extends EventEmitter {
         }
     }
 
-    private cacheHasTitle(url: string, title: string): boolean {
+    private cached(url: string, title: string): boolean {
+        if (!this.cache[url]) return false
         return this.cache[url].last === title || this.cache[url].second === title
     }
 }
